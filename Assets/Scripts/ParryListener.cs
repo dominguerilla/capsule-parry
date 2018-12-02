e18b2efc869aa531af9192abfe35ae641424ed21
Carlos Dominguez <dominguerilla@gmail.com>
2018-12-02 13:11:02 -0500

    init

---
+++ Assets/Scripts/ParryListener.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParryListener : MonoBehaviour {
+
+    public ParryableWeapon weapon;
+
+	void Start () {
+	    weapon.OnParried.AddListener(OnParried);
+	}
+
+    public void OnParried(){
+        Debug.Log("Weapon parried!");
+    }
+
+    public void StartParryListen(){
+        weapon.StartParryListen();
+    }
+
+    public void StopParryListen(){
+        weapon.StopParryListen();
+    }
+}
---
+++ Assets/Scripts/ParryableWeapon.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class ParryableWeapon : MonoBehaviour {
+    public UnityEvent OnParried;
+    public Transform weaponStart, weaponEnd;
+
+    [SerializeField]
+    float capsuleRadius = 0.25f;
+
+    bool isParryable; // true during the parry window of the animation
+    bool wasParried; // true if this weapon was parried in this frame
+    Vector3 lastPosition;
+
+	void Start () {
+		OnParried = new UnityEvent();
+	}
+	
+    // start listening for a parry.
+    // weapons can be parried only once per 'listen'.
+    // be sure to call StopParryListen() to reset the parry counter.
+    public void StartParryListen(){
+        isParryable = true;
+        lastPosition = this.transform.position;
+    }
+
+    public void StopParryListen(){
+        isParryable = false;
+        wasParried = false;
+    }
+
+    private void Update() {
+        if(isParryable){
+            CheckForParry();
+        }
+    }
+
+    private void FixedUpdate() {
+        lastPosition = this.transform.position;
+    }
+
+    void CheckForParry(){
+        int layerMask = LayerMask.GetMask("Parryable");
+        float distance = Vector3.Distance(this.transform.position, lastPosition);
+        Vector3 direction = this.transform.position - lastPosition;
+        RaycastHit hit;
+
+        // for debug purposes
+        Vector3 midpoint = (weaponStart.position + weaponEnd.position) / 2;
+        Debug.DrawRay(midpoint, -direction, Color.red);
+
+        if (Physics.CapsuleCast(weaponStart.position, weaponEnd.position, capsuleRadius, -direction, out hit,distance, layerMask, QueryTriggerInteraction.Collide)){
+            if(!wasParried){
+                OnParried.Invoke();
+                wasParried = true;
+            }
+        }
+
+    }
+}
